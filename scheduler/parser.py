f65f05d40f339bff4e1e9784a2718aeb517c500a
Parth Patel <parth@Parths-MacBook-Pro.local>
2018-02-24 15:20:20 -0500

    added google form parsing

--- scheduler/parser.py
+++ scheduler/parser.py
@@ -1,79 +1,104 @@
 import csv
 from datetime import datetime
 from person import Person
+from person import Conflict
 from course import Course
+from parserConstants import *
+
 
 def sanitizeList(text):
-  return [x for x in (map(lambda x:x.strip(), text.split(','))) if x != '']
+    return [x for x in (map(lambda x: x.strip(), text.split(','))) if x != '']
+
 
 def parseTime(time, formats):
-  # try different formats (sometimes it's '9:30AM', sometimes '9:30 AM')
-  for format in formats:
-    try:
-      return datetime.strptime(time, format).time()
-    except ValueError:
-      pass
-  return 'N/A'
+    # try different formats (sometimes it's '9:30AM', sometimes '9:30 AM')
+    for format in formats:
+        try:
+            return datetime.strptime(time, format).time()
+        except ValueError:
+            pass
+    return 'N/A'
+
+
+def getConflicts(row, fields):
+    index = 0
+    conflicts = []
+    if (row[fields[TIME_CONFLICT]] == "NO"):
+        return conflicts
+    i = 1
+    while True:
+        day = row[fields[CONFLICT_DAY + str(i)]]
+        time = row[fields[CONFLICT_TIME + str(i)]].split("-")
+        conflict = Conflict(day, time[0], time[1])
+        conflicts.append(conflict)
+        nextval = ADDITIONAL_CONFLICT + str(i)
+        if row[fields[nextval]] == "No":
+            break
+        i += 1;
+    return conflicts
+
 
 def parsePeople(path):
-  people = []
-  fields = {}
-  with open(path, newline='') as file:
-    reader = csv.reader(file)
-    headers = next(reader)
-    for i, field in enumerate(headers):
-      fields[field] = i
-    print(headers)
-    for row in reader:
-      # if has a datetime and are returning next semester, then we create person object
-      if row[0] and row[fields['Will you be returning in Spring 2018?']] != 'No':
-        teachPrefs = {}
-        teachPrefs[1] = sanitizeList(row[fields['teachPref1']])
-        teachPrefs[2] = sanitizeList(row[fields['teachPref2']])
-        person = Person(row[fields['Name']],                                                    # name
-                        6 if row[fields['What year will you be in Spring 2018?']] == '>5'       # year
-                          else int(row[fields['What year will you be in Spring 2018?']]),
-                        sanitizeList(row[fields['Qualifying exams passed to date?']]),          # exams
-                        -1 if not row[fields['currentHours']].strip().isnumeric()               # current hours
-                          else int(row[fields['currentHours']].strip()),
-                        -1 if not row[fields['availableHours']].strip().isnumeric()             # available hours
-                          else int(row[fields['availableHours']].strip()),
-                        teachPrefs,                                                             # teaching preferences
-                        sanitizeList(row[fields['assistPref']]),                                # assisting preferences
-                        sanitizeList(row[fields['recitationPref']]),                            # recitation preferences
-                        [x for x in ['assisting', 'teaching', 'recitation', 'Math Help Center'] # category preferences
-                          if x in row[fields['categoryPref']]],
-                        [x for x in ['assisting', 'teaching', 'recitation', 'Math Help Center'] # category least preferences
-                          if x in row[fields['categoryLeastPref']]]
-                        )
-        print(person.categoryLeastPrefs)
-        people.append(person)
-  return people
+    people = []
+    fields = {}
+    with open(path, newline='') as file:
+        reader = csv.reader(file)
+        headers = next(reader)
+        for index, field in enumerate(headers):
+            fields[field] = index
+        for row in reader:
+            # if has a datetime and are returning next semester, then we create person object
+            if row[0] and row[fields[RETURNING_2018]] != 'No':
+                # get all data besides the conflicts
+                teachPrefs = {}
+                name = row[fields[NAME]]
+                yearInSchool = int(row[fields[YEAR]])
+                exams = sanitizeList(row[fields[EXAMS]])
+                currHours = int(row[fields[CURRENT_HOURS]])
+                availHours = int(row[fields[AVAILABLE_HOURS]])
+                teachPrefs[1] = row[fields[TEACHING_PREF_1]].split(",")
+                teachPrefs[2] = row[fields[TEACHING_PREF_2]].split(",")
+                assitancePref = row[fields[ASSISTANCE_PREF]].split(",")
+                recitationPref = row[fields[RECITATION_PREF]].split(",")
+                categoryPref = row[fields[PREFERED_CATEGORY]].split(",")
+                leastPrefCategory = row[fields[LEAST_PREFERED_CATEGORY]].split(",")
+                conflicts = getConflicts(row, fields)
+                person = Person(name, yearInSchool, exams, currHours, availHours,
+                                teachPrefs, assitancePref, recitationPref,
+                                categoryPref, leastPrefCategory, conflicts)
+
+                print(person.toString())
+
+                people.append(person)
+    return people
+
 
 def parseCourses(path):
-  courses = []
-  fields = {}
-  with open(path, newline='') as file:
-    reader = csv.reader(file)
-    next(reader)
-    headers = next(reader)
-    for i, field in enumerate(headers):
-      fields[field] = i
-    for row in reader:
-      if row[0]:
-        days = row[fields['Days']].strip()
-        days = [day for day in days if days not in ['TBA', 'TBD', 'HONORS THESIS']]
-        course = Course(row[fields['Cse']].strip(),                                             # course number
-                        row[fields['Sec']].strip(),                                             # section
-                        days,                                                                   # days
-                        parseTime(row[fields['Start Time']].strip(), ['%I:%M %p', '%I:%M%p']),  # start time
-                        parseTime(row[fields['End Time']].strip(), ['%I:%M %p', '%I:%M%p']),    # end time
-                        row[fields['Instructor']].strip())                                      # instructor
-        courses.append(course)
-  return courses
+    courses = []
+    fields = {}
+    with open(path, newline='') as file:
+        reader = csv.reader(file)
+        next(reader)
+        headers = next(reader)
+        for i, field in enumerate(headers):
+            fields[field] = i
+        for row in reader:
+            if row[0]:
+                days = row[fields['Days']].strip()
+                days = [day for day in days if days not in ['TBA', 'TBD', 'HONORS THESIS']]
+                course = Course(row[fields['Cse']].strip(),  # course number
+                                row[fields['Sec']].strip(),  # section
+                                days,  # days
+                                parseTime(row[fields['Start Time']].strip(), ['%I:%M %p', '%I:%M%p']),  # start time
+                                parseTime(row[fields['End Time']].strip(), ['%I:%M %p', '%I:%M%p']),  # end time
+                                row[fields['Instructor']].strip())  # instructor
+                courses.append(course)
+    return courses
+
 
 if __name__ == '__main__':
-  people = parsePeople('./static/data/s2018.csv')
-  courses = parseCourses('./static/data/s2018_schedule.csv')
-  # for course in courses:
-  #   print(course)
+    people = parsePeople('./static/data/formS2018.csv')
+    courses = parseCourses('./static/data/s2018_schedule.csv')
+
+    # for course in courses:
+    #   print(course)
--- scheduler/person.py
+++ scheduler/person.py
@@ -1,23 +1,59 @@
+from datetime import datetime
+
+
 class Person:
 
-  def __init__(self, 
-                name, 
-                year, 
-                exams, 
-                currHours, 
-                availHours, 
-                teachPrefs, 
-                assistPrefs, 
-                recitationPrefs, 
-                categoryPrefs,
-                categoryLeastPrefs):
-    self.name = name
-    self.year = year
-    self.exams = exams
-    self.currHours = currHours
-    self.availHours = availHours
-    self.teachPrefs = teachPrefs
-    self.assistPrefs = assistPrefs
-    self.recitationPrefs = recitationPrefs
-    self.categoryPrefs = categoryPrefs
-    self.categoryLeastPrefs = categoryLeastPrefs
+    def toString(self):
+        str = ("name={%s} , year={%s}, currHours={%s}, availHours={%s}, "
+               "teach={%s} , assist={%s}, recitation={%s}"
+               ", category={%s} , leastCategoryPref={%s}" % (
+                   self.name, self.year, self.currHours, self.availHours,
+                   self.teachPrefs, self.assistPrefs, self.recitationPrefs,
+                   self.categoryPrefs, self.categoryLeastPrefs))
+        conflictStr = "\n"
+        for conflict in self.conflicts:
+            conflictStr += conflict.toString() + "\n"
+        print(str, conflictStr)
+
+    def __init__(self,
+                 name,
+                 year,
+                 exams,
+                 currHours,
+                 availHours,
+                 teachPrefs,
+                 assistPrefs,
+                 recitationPrefs,
+                 categoryPrefs,
+                 categoryLeastPrefs,
+                 conflicts):
+        self.name = name
+        self.year = year
+        self.exams = exams
+        self.currHours = currHours
+        self.availHours = availHours
+        self.teachPrefs = teachPrefs
+        self.assistPrefs = assistPrefs
+        self.recitationPrefs = recitationPrefs
+        self.categoryPrefs = categoryPrefs
+        self.categoryLeastPrefs = categoryLeastPrefs
+        self.conflicts = conflicts
+
+
+class Conflict:
+
+    def toString(self):
+        if hasattr(self, "day"):
+            return ("day={%s}, start={%s}, end={%s}" % (
+                self.day, self.startTime, self.endTime))
+        else:
+            return ""
+
+    def __init__(self,
+                 day
+                 , startTime
+                 , endTime):
+        if datetime.strptime(startTime, "%H:%M") < datetime.strptime(endTime, "%H:%M"):
+            self.day = day
+            self.startTime = datetime.strptime(startTime, "%H:%M").time()
+            self.endTime = datetime.strptime(endTime, "%H:%M").time()
